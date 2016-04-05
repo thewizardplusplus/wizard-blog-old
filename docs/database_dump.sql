34abc33a69336d3b4e66d8a5099b77d7c5895c19
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-04-05 23:45:48 +0300

    Correct the database dump.

---
+++ docs/database_dump.sql
@@ -0,0 +1,24 @@
+SET NAMES 'utf8';
+SET character_set_client = 'utf8';
+SET character_set_connection = 'utf8';
+SET character_set_results = 'utf8';
+SET character_set_server = 'utf8';
+SET collation_connection = 'utf8_general_ci';
+SET collation_server = 'utf8_general_ci';
+ALTER DATABASE `<имя_базы_данных>`
+	DEFAULT CHARACTER SET = utf8
+	DEFAULT COLLATE = utf8_general_ci;
+
+CREATE TABLE `<имя_базы_данных>`.`posts` (
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`title` VARCHAR(24) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`text` VARCHAR(10000) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`create_time`
+		VARCHAR(19) CHARACTER SET utf8 COLLATE utf8_general_ci
+		NOT NULL,
+	`modify_time`
+		VARCHAR(19) CHARACTER SET utf8 COLLATE utf8_general_ci
+		NOT NULL
+) ENGINE = MyISAM
+	DEFAULT CHARACTER SET = utf8
+	DEFAULT COLLATE = utf8_general_ci;
