a787544390211ee8f8cc0285952100ca7bdd4483
thewizardplusplus <thewizardplusplus@yandex.ru>
2016-04-11 12:06:27 +0300

    Add default parameters

--- parameters.php
+++ parameters.php
@@ -1,9 +1,9 @@
 <?php
-	define("BLOG_USER_NAME",         "");
-	define("BLOG_PASSWORD_MD5_HASH", "");
+	define("BLOG_USER_NAME",         "admin");
+	define("BLOG_PASSWORD_MD5_HASH", "21232f297a57a5a743894a0e4a801fc3");
 
-	define("MYSQL_SERVER",        "");
-	define("MYSQL_LOGIN",         "");
+	define("MYSQL_SERVER",        "localhost");
+	define("MYSQL_LOGIN",         "root");
 	define("MYSQL_PASSWORD",      "");
-	define("MYSQL_DATABASE_NAME", "");
+	define("MYSQL_DATABASE_NAME", "blog_old");
 ?>
